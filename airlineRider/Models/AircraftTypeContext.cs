129da4d3f82a4669b90bcbd1cb515d6d319ab5c2
Tarjei Svendsen <tarjeisvendsen@outlook.com>
2026-08-31 12:47:32 +0200

    fix: disabled transactions for the DbContext. Fixes not being able to insert new records, due to standalone mongodb not supporting transactions apparently.

--- airlineRider/Models/AircraftTypeContext.cs
+++ airlineRider/Models/AircraftTypeContext.cs
@@ -7,6 +7,7 @@ public class AircraftTypeContext : DbContext
     public AircraftTypeContext(DbContextOptions<AircraftTypeContext> options)
         : base(options)
     {
+        Database.AutoTransactionBehavior = AutoTransactionBehavior.Never;
     }
 
     public DbSet<AircraftType> AircraftTypes { get; set; } = null!;
