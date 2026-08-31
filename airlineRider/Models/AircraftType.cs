83028955ea6268abe6a29ae313ea7f59470638fe
Tarjei Svendsen <tarjeisvendsen@outlook.com>
2026-08-31 12:46:05 +0200

    feat: added livery info to AircraftType

--- airlineRider/Models/AircraftType.cs
+++ airlineRider/Models/AircraftType.cs
@@ -12,18 +12,28 @@ public class AircraftType
     [MaxLength(30)]
     public string Model { get; set; }
     [MaxLength(4)]
-    public string Iata { get; set; }
+    public string? Iata { get; set; }
     [MaxLength(4)]
     public string? Icao { get; set; }
     [MaxLength(100)]
     public string Manufacturer { get; set; }
+    
+    public LiveryInfo? LiveryInfo { get; set; }
 
-    public AircraftType(String model, String iata, String icao,String manufacturer)
+
+    public AircraftType()
+    {
+        
+    }
+    public AircraftType(String model, String iata, String icao,String manufacturer,LiveryInfo liveryInfo)
     {
         this.Id = Guid.CreateVersion7();
         this.Model = model;
         this.Iata = iata;
         this.Icao = icao;
         this.Manufacturer = manufacturer;
+        this.LiveryInfo = liveryInfo;
     }
 }
+
+public record LiveryInfo(string WorkPath,string Preview){}
