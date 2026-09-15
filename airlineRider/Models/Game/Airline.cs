63e617565723413878e69a0a16e0d0e0002a75e6
Tarjei Svendsen <tarjeisvendsen@outlook.com>
2026-09-16 00:02:25 +0200

    refactor: made some final tweaks to primitive airline entity.
    will be getting some love in the next pr.

--- airlineRider/Models/Game/Airline.cs
+++ airlineRider/Models/Game/Airline.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 using airlineRider.Models.Auth;
@@ -6,11 +7,14 @@ using Microsoft.EntityFrameworkCore;
 namespace airlineRider.Models.Game;
 
 [PrimaryKey("Id")]
+[Microsoft.EntityFrameworkCore.Index(nameof(Name))]
 public class Airline
 {
     [Column(TypeName = "uuid")]
     public Guid Id { get; set; }
-    public string Name { get; set; }
+
+    [MaxLength(40)]
+    public string Name { get; set; } = string.Empty;
     
     public int LobbyId { get; set; }
     public Lobby Lobby { get; set; }
@@ -20,3 +24,8 @@ public class Airline
     [JsonIgnore]
     public SystemUser SystemUser { get; set; }
 }
+
+[ComplexType]
+public record AirlineFinances(Int64 CurrentBalance);
+[ComplexType]
+public record AircraftLease(int MonthlyCost,DateTime ContractStart,DateTime ContractEnd);
